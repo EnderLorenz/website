7d66316836fff6f6bc9702966bce7a480e38de25
Christopher Newey <christophernewey@christophersmbp.attlocal.net>
2020-09-18 18:22:06 -0500

    bsttrab

--- algos/bst/sketch.js
+++ algos/bst/sketch.js
@@ -1,5 +1,6 @@
-var tree;
-let input, input2, button, button2;
+var tree, count;
+let input, input2, button, button2, button3, button4, button5, button6, button7;
+
 
 function setup() {
   canvas = createCanvas(2*windowWidth, windowHeight);
@@ -9,9 +10,9 @@ function setup() {
   for (var i = 0; i < 15; i++) {
     tree.addValue(floor(random(0,1000)))
   }
-  tree.traverse();
+  tree.drawTraverse();
   input = createInput('').attribute('placeholder', 'Positive Integers');
-  input.position(windowWidth-input.width-10, 100,);
+  input.position(windowWidth-input.width-20, 100,);
   button = createButton('Search');
   button.position(input.x, input.y+input.height);
   button.mousePressed(search);
@@ -23,6 +24,22 @@ function setup() {
   button3 = createButton('Delete');
   button3.position(button2.x+button2.width, input.y+input.height);
   button3.mousePressed(removeNode);
+
+  button4 = createButton('invert');
+  button4.position(button.x, button.y+button.height);
+  button4.mousePressed(invertTree);
+
+  button5 = createButton('pre');
+  button5.position(button.x + button4.width, button.y+button.height);
+  button5.mousePressed(preTraverseTree);
+
+  button6 = createButton('in');
+  button6.position(button5.x + button5.width, button.y+button.height);
+  button6.mousePressed(inTraverseTree);
+
+  button7 = createButton('post');
+  button7.position(button6.x + button6.width, button.y+button.height);
+  button7.mousePressed(postTraverseTree);
 }
 
 function search() {
@@ -31,13 +48,13 @@ function search() {
     input.value('');
     if (!isInt(num)) {
       background(175);
-      tree.traverse();
+      tree.drawTraverse();
       input.attribute('placeholder', 'Not an integer');
     } else {
       num = parseFloat(num);
       background(175);
-      tree.traverse();
-      result = tree.search(num);
+      tree.drawTraverse();
+      result = tree.drawSearch(num);
       if (result === null) {
         input.attribute('placeholder', 'Not found');
       } else {
@@ -53,16 +70,16 @@ function add() {
   var num = input.value();
   input.value('');
   if (!isInt(num)) {
-    tree.traverse();
+    tree.drawTraverse();
     input.attribute('placeholder', 'Not an integer');
   } else if (tree.root) {
       num = parseFloat(num);
-      tree.traverse();
-      result = tree.quietSearch(num);
+      tree.drawTraverse();
+      result = tree.search(num);
       if (result === null) {
         tree.addValue(num);
         background(175);
-        tree.traverse();
+        tree.drawTraverse();
         input.attribute('placeholder', 'Added!!');
       } else {
         input.attribute('placeholder', 'Not unique');
@@ -71,32 +88,71 @@ function add() {
     num = parseFloat(num);
     tree.addValue(num);
     input.attribute('placeholder', 'Added!!');
-    tree.traverse();
+    tree.drawTraverse();
   }
 }
 
 function removeNode() {
   background(175);
   if (tree.root) {
-    tree.traverse();
+    tree.drawTraverse();
     var num = input.value();
     input.value('');
     if (!isInt(num)) {
       input.attribute('placeholder', 'Not an integer');
     } else {
       num = parseFloat(num);
-      result = tree.quietSearch(num);
+      result = tree.search(num);
       if (result === null) {
         input.attribute('placeholder', 'Nothing Removed');
       } else {
         tree.remove(num);
         background(175);
         if (tree.root) {
-          tree.traverse();
+          tree.drawTraverse();
         input.attribute('placeholder', 'Removed');
         }
       }
     }
+  } else input.attribute('placeholder', 'Make a tree');
+}
+
+function invertTree() {
+  if (tree.root) {
+      background(175);
+      tree.invert();
+      tree.drawTraverse();
+      input.attribute('placeholder', 'Inverted!!');
+  } else input.attribute('placeholder', 'Make a tree');
+}
+
+function preTraverseTree() {
+  if (tree.root) {
+      background(175);
+      tree.drawTraverse();
+      count = 0;
+      tree.traverse('pre')
+      input.attribute('placeholder', 'Traversing!!');
+  } else input.attribute('placeholder', 'Make a tree');
+}
+
+function inTraverseTree(type) {
+  if (tree.root) {
+      background(175);
+      tree.drawTraverse();
+      count = 0;
+      tree.traverse('in')
+      input.attribute('placeholder', 'Traversing!!');
+  } else input.attribute('placeholder', 'Make a tree');
+}
+
+function postTraverseTree() {
+  if (tree.root) {
+      background(175);
+      tree.drawTraverse();
+      count = 0;
+      tree.traverse('post')
+      input.attribute('placeholder', 'Traversing!!');
   } else input.attribute('placeholder', 'Make a tree');
 }
 
--- algos/bst/tree.js
+++ algos/bst/tree.js
@@ -1,35 +1,49 @@
 function Tree() {
     this.root = null;
 }
-  
-  Tree.prototype.traverse = function() {
-    this.root.visit(this.root);
-  }
-  
-  Tree.prototype.search = function(val) {
-      var found = this.root.search(val);
-      return found;
-  }
-
-  Tree.prototype.quietSearch = function(val) {
-    var found = this.root.quietSearch(val);
-    return found;
-}
 
   Tree.prototype.addValue = function(val) {
     var n = new Node(val);
     if (this.root == null) {
       this.root = n;
       this.root.order = 140;
-      this.root.dy =  -0;
-      this.root.dx = 0.00001;
       this.root.angle1 = -PI/2.3;
       this.root.angle2 = PI/2.3;
+      this.num = 0;
     } else {
-      this.root.addNode(n) 
+      this.root.addNode(n)
     }
   }
 
   Tree.prototype.remove = function(data) {
       this.root = this.root.removeNode(this.root, data)
+  }  
+  
+  Tree.prototype.search = function(val) {
+    var found = this.root.search(val);
+    return found;
   }
+
+  Tree.prototype.traverse = async function(type) {
+    await Promise.all([this.root.traverse(type)]);
+  }
+
+  Tree.prototype.invert = function() {
+    this.root.invert();
+  }
+    
+  Tree.prototype.drawTraverse = function() {
+     this.root.drawTraverse();
+  }
+
+  Tree.prototype.drawSearch = function(val) {
+      var found = this.root.drawSearch(val);
+      return found;
+  }
+
+  Tree.prototype.sleep = function(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+
+
