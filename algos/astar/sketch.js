6fc3a587fc94757065de1d31bcd76307574b35ae
Christopher Newey <christophernewey@christophersmbp.attlocal.net>
2020-09-26 22:07:58 -0500

    button

--- algos/astar/sketch.js
+++ algos/astar/sketch.js
@@ -1,6 +1,6 @@
 var cols = 150;
 var rows = 150;
-var w, h, button;
+var w, h, button, button2;
 var aStarBoolean = true;
 var dBoolean = false;
 
@@ -11,14 +11,17 @@ function setup() {
 
   canvas = createCanvas(canvasSize-90, canvasSize-90);
   canvas.parent('img');
-  w = width / cols;
-  h = height / rows;
+  w = (width + 1) / cols;
+  h = (height + 1) / rows;
   background(0);
-  button = createButton('A*: Press to change to Djkstra');
+  button = createButton('A');
   button.parent('img')
-  button.size(230, 21)
-  button.position(50, 210);
-  button.mousePressed(switchType);
+  button.position(50, 205);
+  button.mousePressed(aStarPressed);
+  button2 = createButton('Djkstra');
+  button2.parent('img')
+  button2.position(button.x+button.width, 205);
+  button2.mousePressed(aStarPressed);
 
 
   astar = new PathFinder;
@@ -34,25 +37,15 @@ function draw() {
   
 }
 
-function switchType() {
-  if (aStarBoolean) {
+function aStarPressed() {
     aStarBoolean = false;
     dBoolean = true;
     astar = new PathFinder;
-    button = createButton('Djkstra: Press to change to A*');
-    button.parent('img')
-    button.size(230, 21)
-    button.position(50, 210);
-    button.mousePressed(switchType);
-  } else {
+}
+
+function dPressed() {
     dBoolean = false;
     astar = new PathFinder;
     aStarBoolean = true;
-    button = createButton('A*: Press to change to Djkstra');
-    button.parent('img')
-    button.size(230, 21)
-    button.position(50, 210);
-    button.mousePressed(switchType);
-  }
-  
 }
+
