17fe5c27f82e7ce49695da0de2923932091c6f4e
Christopher Newey <christophernewey@christophersmbp.attlocal.net>
2020-09-26 20:32:12 -0500

    both

---
+++ algos/astar/pathfinders.js
@@ -0,0 +1,192 @@
+function PathFinder() {
+    this.openSet = [];
+    this.closedSet = [];
+    this.grid = new Array(cols);
+    this.start;
+    this.end;
+    this.w
+    this.h;
+    this.path = [];
+
+    for (var i = 0; i < cols; i++) {
+        this.grid[i] = new Array(rows)
+    }
+    for (var i = 0; i < cols; i++) {
+        for (var j = 0; j < rows; j++) {
+            this.grid[i][j] = new Spot(i, j);
+        }
+    }
+    for (var i = 0; i < cols; i++) {
+        for (var j = 0; j < rows; j++) {
+            this.grid[i][j].addNeighbors(this.grid);
+        }
+    }
+    this.start = this.grid[floor(rows/2)][floor(cols/2)];
+    this.end = this.grid[cols - 1][rows - 1];
+    this.start.wall = false;
+    this.end.wall = false;
+    this.openSet.push(this.start);
+
+    this.aStarOpen = function() {
+        if (this.openSet.length > 0) {
+            var winner = 0;
+            for (var i = 0; i < this.openSet.length; i++) {
+              if (this.openSet[i].f < this.openSet[winner].f) winner = i;
+            }
+            var current = this.openSet[winner];
+        
+            if (current === this.end) {
+              noLoop();
+              console.log("Done!");
+            }
+        
+            this.removeFromArray(this.openSet, current)
+            this.closedSet.push(current);
+        
+            var neighbors = current.neighbors;
+            for (var i = 0; i < neighbors.length; i++) {
+              var neighbor = neighbors[i];
+        
+              if (!this.closedSet.includes(neighbor) && !neighbor.wall) {
+                var tempG = current.g + 1;
+        
+                var newPath = false;
+                if (this.openSet.includes(neighbor)) {
+                  if (tempG < neighbor.g) {
+                    neighbor.g = tempG;
+                    newPath = true;
+                  }
+                } else {
+                  neighbor.g = tempG;
+                  newPath = true;
+                  this.openSet.push(neighbor);
+                }
+        
+                if (newPath) {
+                  neighbor.h = this.heuristic(neighbor, this.end);
+                  neighbor.f = neighbor.g + neighbor.h;
+                  neighbor.previous = current;
+                }
+              }
+            }
+          } else {
+                console.log('no soln')
+                noLoop();
+                return;
+          }
+        
+          for (var i = 0; i < cols; i++) {
+            for (var j = 0; j < rows; j++) {
+                this.grid[i][j].show(color(255))
+            }
+          }
+          for (var i = 0; i < this.closedSet.length; i++) {
+            this.closedSet[i].show(color(255, 0, 0))
+          }
+          for (var i = 0; i < this.openSet.length; i++) {
+            this.openSet[i].show(color(0, 255, 0))
+          }
+        
+          //find the path
+            this.path = [];
+            var tmp = current;
+            this.path.push(tmp);
+        
+            while (tmp.previous) {
+                this.path.push(tmp.previous);
+                tmp = tmp.previous;
+            }
+        
+          for (var i = 0; i < this.path.length; i++) {
+            this.path[i].show(color(0, 0, 255));
+          }
+    };
+
+    this.dijkstraOpen = function() {
+        if (this.openSet.length > 0) {
+            var winner = 0;
+            for (var i = 0; i < this.openSet.length; i++) {
+              if (this.openSet[i].f < this.openSet[winner].f) winner = i;
+            }
+            var current = this.openSet[winner];
+        
+            if (current === this.end) {
+              noLoop();
+              console.log("Done!");
+            }
+        
+            this.removeFromArray(this.openSet, current)
+            this.closedSet.push(current);
+        
+            var neighbors = current.neighbors;
+            for (var i = 0; i < neighbors.length; i++) {
+              var neighbor = neighbors[i];
+        
+              if (!this.closedSet.includes(neighbor) && !neighbor.wall) {
+                var tempG = current.g + 1;
+        
+                var newPath = false;
+                if (this.openSet.includes(neighbor)) {
+                  if (tempG < neighbor.g) {
+                    neighbor.g = tempG;
+                    newPath = true;
+                  }
+                } else {
+                  neighbor.g = tempG;
+                  newPath = true;
+                  this.openSet.push(neighbor);
+                }
+        
+                if (newPath) {
+                  neighbor.h = 0;
+                  neighbor.f = neighbor.g + neighbor.h;
+                  neighbor.previous = current;
+                }
+              }
+            }
+          } else {
+                console.log('no soln')
+                noLoop();
+                return;
+          }
+        
+          for (var i = 0; i < cols; i++) {
+            for (var j = 0; j < rows; j++) {
+                this.grid[i][j].show(color(255))
+            }
+          }
+          for (var i = 0; i < this.closedSet.length; i++) {
+            this.closedSet[i].show(color(255, 0, 0))
+          }
+          for (var i = 0; i < this.openSet.length; i++) {
+            this.openSet[i].show(color(0, 255, 0))
+          }
+        
+          //find the path
+            this.path = [];
+            var tmp = current;
+            this.path.push(tmp);
+        
+            while (tmp.previous) {
+                this.path.push(tmp.previous);
+                tmp = tmp.previous;
+            }
+        
+          for (var i = 0; i < this.path.length; i++) {
+            this.path[i].show(color(0, 0, 255));
+          }
+    };
+
+    this.removeFromArray = function(arr, elt) {
+        for (var i = arr.length - 1; i >= 0; i--) {
+          if (arr[i] == elt) {
+            arr.splice(i, 1);
+          }
+        }
+    };
+      
+    this.heuristic = function(a, b) {
+        var d = dist(a.i, a.j, b.i, b.j);
+        return d;
+    };
+}
--- algos/astar/sketch.js
+++ algos/astar/sketch.js
@@ -1,25 +1,8 @@
-function removeFromArray(arr, elt) {
-  for (var i = arr.length - 1; i >= 0; i--) {
-    if (arr[i] == elt) {
-      arr.splice(i, 1);
-    }
-  }
-}
-
-function heuristic(a, b) {
-  var d = dist(a.i, a.j, b.i, b.j);
-  return d;
-}
-
 var cols = 150;
 var rows = 150;
-var grid = new Array(cols);
-var openSet = [];
-var closedSet = [];
-var start;
-var end;
-var w, h;
-var path = [];
+var w, h, button;
+var aStarBoolean = true;
+var dBoolean = false;
 
 function setup() {
   if (windowWidth < windowHeight) {
@@ -28,119 +11,48 @@ function setup() {
 
   canvas = createCanvas(canvasSize-90, canvasSize-90);
   canvas.parent('img');
-  console.log('A*');
   w = width / cols;
   h = height / rows;
   background(0);
-  for (var i = 0; i < cols; i++) {
-    grid[i] = new Array(rows)
-  }
-  for (var i = 0; i < cols; i++) {
-    for (var j = 0; j < rows; j++) {
-      grid[i][j] = new Spot(i, j);
-    }
-  }
-  for (var i = 0; i < cols; i++) {
-    for (var j = 0; j < rows; j++) {
-      grid[i][j].addNeighbors(grid);
-    }
-  }
-  start = grid[0][0];
-  end = grid[cols - 1][rows - 1];
-  start.wall = false;
-  end.wall = false;
-  openSet.push(start);
+  button = createButton('A*: Press to change to Djkstra');
+  button.parent('img')
+  button.size(230, 21)
+  button.position(50, 210);
+  button.mousePressed(switchType);
+
+
+  astar = new PathFinder;
 }
 
 function draw() {
-  //draw is looping 
-  if (openSet.length > 0) {
-    //keep looping
-    var winner = 0;
-    for (var i = 0; i < openSet.length; i++) {
-      if (openSet[i].f < openSet[winner].f) {
-        winner = i;
-      }
-    }
-    var current = openSet[winner];
+  if (aStarBoolean) {
+    astar.aStarOpen();
+  }
+  if (dBoolean) {
+    astar.dijkstraOpen();
+  }
+  
+}
 
-    if (current === end) {
-      noLoop();
-      console.log("Done!");
-    }
-
-    removeFromArray(openSet, current)
-    closedSet.push(current);
-
-    var neighbors = current.neighbors;
-    for (var i = 0; i < neighbors.length; i++) {
-      var neighbor = neighbors[i];
-
-      if (!closedSet.includes(neighbor) && !neighbor.wall) {
-        var tempG = current.g + 1;
-
-        var newPath = false;
-        if (openSet.includes(neighbor)) {
-          if (tempG < neighbor.g) {
-            neighbor.g = tempG;
-            newPath = true;
-          }
-        } else {
-          neighbor.g = tempG;
-          newPath = true;
-          openSet.push(neighbor);
-        }
-
-        if (newPath) {
-          neighbor.h = heuristic(neighbor, end);
-          neighbor.f = neighbor.g + neighbor.h;
-          neighbor.previous = current;
-        }
-      }
-    }
-
+function switchType() {
+  if (aStarBoolean) {
+    aStarBoolean = false;
+    dBoolean = true;
+    astar = new PathFinder;
+    button = createButton('Djkstra: Press to change to A*');
+    button.parent('img')
+    button.size(230, 21)
+    button.position(50, 210);
+    button.mousePressed(switchType);
   } else {
-    console.log('no soln')
-    noLoop();
-    return;
-    
+    dBoolean = false;
+    astar = new PathFinder;
+    aStarBoolean = true;
+    button = createButton('A*: Press to change to Djkstra');
+    button.parent('img')
+    button.size(230, 21)
+    button.position(50, 210);
+    button.mousePressed(switchType);
   }
-
-  for (var i = 0; i < cols; i++) {
-    for (var j = 0; j < rows; j++) {
-      grid[i][j].show(color(255))
-    }
-  }
-  for (var i = 0; i < closedSet.length; i++) {
-    closedSet[i].show(color(255, 0, 0))
-  }
-  for (var i = 0; i < openSet.length; i++) {
-    openSet[i].show(color(0, 255, 0))
-  }
-
-  //find the path
-    path = [];
-    var tmp = current;
-    path.push(tmp);
-
-    while (tmp.previous) {
-      path.push(tmp.previous);
-      tmp = tmp.previous;
-    }
-
-  for (var i = 0; i < path.length; i++) {
-    path[i].show(color(0, 0, 255));
-  }
-
-}
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-  centerCanvas();
-}
-
-function centerCanvas() {
-  var x = (windowWidth - width) / 2;
-  var y = (windowHeight - height) / 2;
-  canvas.position(x, y);
+  
 }
